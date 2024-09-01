b8a3f701ba1914af97f3b6747ae184ff63a5d87b
SebaSubi <sebaperezlavooy@gmail.com>
2024-09-01 13:59:06 -0300

    Added sequence for a inline 6 and a inline 4

--- src/utils/Random.ts
+++ src/utils/Random.ts
@@ -2,4 +2,8 @@ export default class Random {
   static get() {
     return Math.floor(Math.random() * 1000)
   }
+
+  static randomWithParams(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min) + 1) + 1
+  }
 }
